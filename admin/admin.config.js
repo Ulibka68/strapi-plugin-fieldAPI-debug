1113883f5400e648142fa6743680406e6bf5b31e
Ulibka68 <gena68@inbox.ru>
2021-04-05 08:55:05 +0300

    проблема - не обрабатывался node_modules - решена

--- admin/admin.config.js
+++ admin/admin.config.js
@@ -1,62 +1,17 @@
+const path = require("path");
+
 module.exports = {
   webpack: (config, webpack) => {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
     // Important: return the modified config
-
-    // Allow scss modules
-    // eslint-disable-next-line no-param-reassign
-    config.resolve = {
-      ...config.resolve,
-      extensions: [...config.resolve.extensions, ".scss"],
-    };
-
-    // Configure a SASS loader
-    config.module.rules.push({
-      test: /\.s[ac]ss$/i,
-      use: [
-        "style-loader",
-        "css-loader",
-        "sass-loader",
-        {
-          loader: "sass-loader",
-          options: {
-            // eslint-disable-next-line global-require
-            implementation: require("sass"),
-          },
-        },
-      ],
+    const regexToMatch = /\.m?js$/;
+    config.module.rules = config.module.rules.map((rule) => {
+      if (rule.test.toString() == regexToMatch.toString()) {
+        delete rule.exclude;
+      }
+      return rule;
     });
-
-    config.module.rules.push({
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: require.resolve("babel-loader"),
-        options: {
-          cacheDirectory: true,
-          presets: [
-            require.resolve("@babel/preset-env"),
-            require.resolve("@babel/preset-react"),
-          ],
-          plugins: [
-            require.resolve("@babel/plugin-proposal-class-properties"),
-            require.resolve("@babel/plugin-syntax-dynamic-import"),
-            require.resolve("@babel/plugin-transform-modules-commonjs"),
-            require.resolve("@babel/plugin-proposal-async-generator-functions"),
-            [
-              require.resolve("@babel/plugin-transform-runtime"),
-              {
-                helpers: true,
-                regenerator: true,
-              },
-            ],
-          ],
-        },
-      },
-    });
-
-    console.log(config.module.rules);
 
     return config;
   },
